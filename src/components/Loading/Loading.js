8b7ac0c271490930acb14361c7f6b725a5c2d783
jamieomaguire <jamieolivermaguire@googlemail.com>
2017-04-25 21:38:30 +0100

    added a loader animation

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -5,7 +5,11 @@ import styles from './Loading.scss'
 // A quick and simple message to assure people that the application is loading
 const Loading = () => (
   <section className={styles.container} aria-labelledby='Loading content'>
-    <p>Loading the instances...</p>
+    <div className={styles.dots}>
+      <span className={styles.dot}></span>
+      <span className={styles.dot}></span>
+      <span className={styles.dot}></span>
+    </div>
   </section>
 )
 
